4144be4a75c50325fc0c712f74e83d135716b2ba
Noahnc <nc.canadea@gmail.com>
2021-09-19 14:08:54 +0200

    Update

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,7 +11,11 @@ import javafx.scene.layout.VBox;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import javafx.scene.text.Font;
+import logic.BitcoinHandler;
 
 public class MainController implements Initializable {
 
@@ -20,6 +25,39 @@ public class MainController implements Initializable {
 
         File image = new File("src/assets/bitcoin.png");
         MainWindowsImage.setImage(new Image(image.toURI().toString()));
+
+    }
+
+    @FXML
+    public void StartTracing() {
+        InputInfo.setText("");
+        ArrayList<String> BitcoinAddresses = null;
+        Label BitcoinAddressText;
+        TransactionsVBox.getChildren().removeAll(TransactionsVBox.getChildren());
+        BitcoinHandler Bitcoinhandler = new BitcoinHandler();
+        String BitcoinAddress = BitcoinAddressValue.getText();
+
+        if (BitcoinAddress.equals("")) {
+            InputInfo.setText("Eingabe darf nicht leer sein!");
+        } else {
+            if (Bitcoinhandler.CheckIfBitcoinAddressIsValid(BitcoinAddress)) {
+                BitcoinAddresses = Bitcoinhandler.getBitcoinAddresses(BitcoinAddress);
+
+                if (BitcoinAddresses.size() == 0) {
+                    InputInfo.setText("Unter dieser Bitcoin Adresse wurde keine ausgehende Transaktion gefunden.");
+                } else {
+                    for (var address : BitcoinAddresses) {
+                        BitcoinAddressText = new Label(address);
+                        BitcoinAddressText.setFont(new Font("Arial", 20));
+                        TransactionsVBox.getChildren().add(BitcoinAddressText);
+
+                    }
+                }
+
+            } else {
+                InputInfo.setText("Keine gültige Bitcoin Adresse eingegeben!");
+            }
+        }
 
     }
 
@@ -34,5 +72,7 @@ public class MainController implements Initializable {
 
     @FXML
     protected VBox TransactionsVBox;
+    @FXML
+    protected Label InputInfo;
 
 }
---
+++ src/main/java/logic/BitcoinHandler.java
@@ -0,0 +1,25 @@
+package logic;
+
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+public class BitcoinHandler {
+
+    public boolean CheckIfBitcoinAddressIsValid(String BitcoinAddress) {
+        boolean isValid = true;
+
+
+        return isValid;
+    }
+
+    public ArrayList getBitcoinAddresses(String BitcoinAddress) {
+        ArrayList<String> BitcoinAddresses = new ArrayList<String>();
+
+
+        return BitcoinAddresses;
+    }
+
+}
+
+
